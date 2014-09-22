e0cd807b13ea4290578e89e181998d6d919f153b
Jared Hoberock <jaredhoberock@gmail.com>
2014-09-22 16:18:41 -0700

    Works now

--- bind.hpp
+++ bind.hpp
@@ -14,6 +14,20 @@ namespace thrust
 {
 namespace experimental
 {
+
+
+template<class T>
+struct is_placeholder : thrust::detail::false_type {};
+
+
+template<unsigned int i>
+struct is_placeholder<
+  thrust::detail::functional::actor<
+    thrust::detail::functional::argument<i>
+  >
+> : thrust::detail::true_type {};
+
+
 namespace detail
 {
 namespace bind_detail
@@ -87,6 +101,7 @@ template<class... _Tp>
 using index_sequence_for = make_index_sequence<sizeof...(_Tp)>;
 
 
+__thrust_hd_warning_disable__
 template<typename F, typename Tuple, size_t... I>
 __host__ __device__
 auto apply_impl(F&& f, Tuple&& t, index_sequence<I...>)
@@ -122,10 +137,12 @@ auto apply(F&& f, Tuple&& t)
 }
 
 
-// XXX should use enable_if is_placeholder
 template<class ArgTuple, class BoundArg>
 __host__ __device__
-auto substitute_arg(ArgTuple&, BoundArg&& bound_arg)
+auto substitute_arg(ArgTuple&&, BoundArg&& bound_arg,
+                    typename thrust::detail::disable_if<
+                      is_placeholder<decay_t<BoundArg>>::value
+                    >::type* = 0)
   -> decltype(
        std::forward<BoundArg>(bound_arg)
      )
@@ -134,32 +151,262 @@ auto substitute_arg(ArgTuple&, BoundArg&& bound_arg)
 }
 
 
-// XXX should use enable_if is_placeholder
-template<class ArgTuple, size_t i>
+template<unsigned int i>
+struct placeholder
+  : thrust::detail::functional::actor<
+      thrust::detail::functional::argument<i>
+    >
+{};
+
+
+template<class T>
+struct argument_index
+  : thrust::detail::integral_constant<
+      unsigned int, 0
+    >
+{};
+
+
+template<unsigned int i>
+struct argument_index<
+  thrust::detail::functional::actor<
+    thrust::detail::functional::argument<i>
+  >
+>
+  : thrust::detail::integral_constant<
+      unsigned int, i
+    >
+{};
+
+
+template<class ArgTuple, class BoundArg>
 __host__ __device__
-auto substitute_arg(ArgTuple&& arg_tuple, const typename thrust::detail::functional::placeholder<i>::type&)
+auto substitute_arg(ArgTuple&& arg_tuple, const BoundArg&,
+                   typename thrust::detail::enable_if<
+                     is_placeholder<decay_t<BoundArg>>::value
+                   >::type* = 0)
   -> decltype(
-       thrust::get<i>(std::forward<ArgTuple>(arg_tuple))
+       thrust::get<
+         argument_index<BoundArg>::value
+       >(std::forward<ArgTuple>(arg_tuple))
      )
 {
-  return thrust::get<i>(std::forward<ArgTuple>(arg_tuple));
+  const unsigned int idx = argument_index<BoundArg>::value;
+  return thrust::get<idx>(std::forward<ArgTuple>(arg_tuple));
+}
+
+
+// XXX WAR nvbug 1527140
+//     unpack template parameter packs into thrust::tuple manually
+template<class... T>
+struct tuple_war_1527140;
+
+template<>
+struct tuple_war_1527140<>
+{
+  using type = thrust::tuple<>;
+};
+
+template<class T1>
+struct tuple_war_1527140<T1>
+{
+  using type = thrust::tuple<T1>;
+};
+
+template<class T1, class T2>
+struct tuple_war_1527140<T1,T2>
+{
+  using type = thrust::tuple<T1,T2>;
+};
+
+template<class T1, class T2, class T3>
+struct tuple_war_1527140<T1,T2,T3>
+{
+  using type = thrust::tuple<T1,T2,T3>;
+};
+
+template<class T1, class T2, class T3, class T4>
+struct tuple_war_1527140<T1,T2,T3,T4>
+{
+  using type = thrust::tuple<T1,T2,T3,T4>;
+};
+
+template<class T1, class T2, class T3, class T4, class T5>
+struct tuple_war_1527140<T1,T2,T3,T4,T5>
+{
+  using type = thrust::tuple<T1,T2,T3,T4,T5>;
+};
+
+template<class T1, class T2, class T3, class T4, class T5, class T6>
+struct tuple_war_1527140<T1,T2,T3,T4,T5,T6>
+{
+  using type = thrust::tuple<T1,T2,T3,T4,T5,T6>;
+};
+
+template<class T1, class T2, class T3, class T4, class T5, class T6, class T7>
+struct tuple_war_1527140<T1,T2,T3,T4,T5,T6,T7>
+{
+  using type = thrust::tuple<T1,T2,T3,T4,T5,T6,T7>;
+};
+
+template<class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8>
+struct tuple_war_1527140<T1,T2,T3,T4,T5,T6,T7,T8>
+{
+  using type = thrust::tuple<T1,T2,T3,T4,T5,T6,T7,T8>;
+};
+
+template<class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9>
+struct tuple_war_1527140<T1,T2,T3,T4,T5,T6,T7,T8,T9>
+{
+  using type = thrust::tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9>;
+};
+
+template<class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10>
+struct tuple_war_1527140<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10>
+{
+  using type = thrust::tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10>;
+};
+
+template<class... T>
+using tuple = typename tuple_war_1527140<T...>::type;
+
+
+// XXX replace this with the variadic forward_as_tuple() when thrust::tuple's constructor can receive && references
+inline __host__ __device__
+tuple<> forward_as_tuple()
+{
+  return tuple<>();
+}
+
+
+template<class T>
+__host__ __device__
+tuple<T&> forward_as_tuple(T& arg)
+{
+  return tuple<T&>(arg);
+}
+
+
+template<class T>
+__host__ __device__
+tuple<const T&> forward_as_tuple(const T& arg)
+{
+  return tuple<const T&>(arg);
+}
+
+
+template<class T1, class T2>
+__host__ __device__
+tuple<T1&,T2&> forward_as_tuple(T1& arg1, T2& arg2)
+{
+  return tuple<T1&,T2&>(arg1, arg2);
+}
+
+
+template<class T1, class T2>
+__host__ __device__
+tuple<T1&,const T2&> forward_as_tuple(T1& arg1, const T2& arg2)
+{
+  return tuple<T1&,const T2&>(arg1, arg2);
+}
+
+
+template<class T1, class T2>
+__host__ __device__
+tuple<const T1&,T2&> forward_as_tuple(const T1& arg1, T2& arg2)
+{
+  return tuple<const T1&,T2&>(arg1, arg2);
+}
+
+
+template<class T1, class T2>
+__host__ __device__
+tuple<const T1&,const T2&> forward_as_tuple(const T1& arg1, const T2& arg2)
+{
+  return tuple<const T1&,const T2&>(arg1, arg2);
+}
+
+
+template<class T1, class T2, class T3>
+__host__ __device__
+tuple<T1&,T2&,T3&> forward_as_tuple(T1& arg1, T2& arg2, T3& arg3)
+{
+  return tuple<T1&,T2&,T3&>(arg1, arg2, arg3);
+}
+
+
+template<class T1, class T2, class T3>
+__host__ __device__
+tuple<T1&,T2&,const T3&> forward_as_tuple(T1& arg1, T2& arg2, const T3& arg3)
+{
+  return tuple<T1&,T2&,const T3&>(arg1, arg2, arg3);
+}
+
+
+template<class T1, class T2, class T3>
+__host__ __device__
+tuple<T1&,const T2&,T3&> forward_as_tuple(T1& arg1, const T2& arg2, T3& arg3)
+{
+  return tuple<T1&,const T2&, T3&>(arg1, arg2, arg3);
+}
+
+
+template<class T1, class T2, class T3>
+__host__ __device__
+tuple<T1&,const T2&,const T3&> forward_as_tuple(T1& arg1, const T2& arg2, const T3& arg3)
+{
+  return tuple<T1&,const T2&,const T3&>(arg1, arg2, arg3);
+}
+
+
+template<class T1, class T2, class T3>
+__host__ __device__
+tuple<const T1&,T2&,T3&> forward_as_tuple(const T1& arg1, T2& arg2, T3& arg3)
+{
+  return tuple<const T1&,T2&,T3&>(arg1, arg2, arg3);
+}
+
+
+template<class T1, class T2, class T3>
+__host__ __device__
+tuple<const T1&,T2&,const T3&> forward_as_tuple(const T1& arg1, T2& arg2, const T3& arg3)
+{
+  return tuple<const T1&,T2&,const T3&>(arg1, arg2, arg3);
+}
+
+
+template<class T1, class T2, class T3>
+__host__ __device__
+tuple<const T1&,const T2&,T3&> forward_as_tuple(const T1& arg1, const T2& arg2, T3& arg3)
+{
+  return tuple<const T1&,const T2&, T3&>(arg1, arg2, arg3);
+}
+
+
+template<class T1, class T2, class T3>
+__host__ __device__
+tuple<const T1&,const T2&,const T3&> forward_as_tuple(const T1& arg1, const T2& arg2, const T3& arg3)
+{
+  return tuple<const T1&,const T2&,const T3&>(arg1, arg2, arg3);
 }
 
 
 template<class ArgTuple, class BoundArgTuple, size_t... I>
 __host__ __device__
-auto substitute_impl(ArgTuple& arg_tuple, BoundArgTuple& bound_arg_tuple, index_sequence<I...>)
+auto substitute_impl(ArgTuple&& arg_tuple, BoundArgTuple&& bound_arg_tuple, index_sequence<I...>)
   -> decltype(
-       thrust::tie(
+       forward_as_tuple(
          substitute_arg(
-           arg_tuple, thrust::get<I>(bound_arg_tuple)
+           std::forward<ArgTuple>(arg_tuple),
+           thrust::get<I>(std::forward<BoundArgTuple>(bound_arg_tuple))
          )...
        )
      )
 {
-  return thrust::tie(
+  return forward_as_tuple(
     substitute_arg(
-      arg_tuple, thrust::get<I>(bound_arg_tuple)
+      std::forward<ArgTuple>(arg_tuple),
+      thrust::get<I>(std::forward<BoundArgTuple>(bound_arg_tuple))
     )...
   );
 }
@@ -167,17 +414,17 @@ auto substitute_impl(ArgTuple& arg_tuple, BoundArgTuple& bound_arg_tuple, index_
 
 template<class ArgTuple, class BoundArgTuple>
 __host__ __device__
-auto substitute(ArgTuple& arg_tuple, BoundArgTuple& bound_arg_tuple)
+auto substitute(ArgTuple&& arg_tuple, BoundArgTuple&& bound_arg_tuple)
   -> decltype(
        substitute_impl(
-         arg_tuple,
-         bound_arg_tuple,
+         std::forward<ArgTuple>(arg_tuple),
+         std::forward<BoundArgTuple>(bound_arg_tuple),
          make_index_sequence<thrust::tuple_size<decay_t<BoundArgTuple>>::value>()
        )
      )
 {
   using Indices = make_index_sequence<thrust::tuple_size<decay_t<BoundArgTuple>>::value>;
-  return substitute_impl(arg_tuple, bound_arg_tuple, Indices());
+  return substitute_impl(std::forward<ArgTuple>(arg_tuple), std::forward<BoundArgTuple>(bound_arg_tuple), Indices());
 }
 
 
@@ -191,58 +438,18 @@ class bind_expression
         bound_args_(bound_args...)
     {}
 
-    // XXX WAR nvcc 6.5 issue
-    __thrust_hd_warning_disable__
-    template<int = 0>
-    __host__ __device__
-    typename std::result_of<F()>::type operator()()
-    {
-      return fun_();
-    }
-
-
-    __thrust_hd_warning_disable__
-    template<class... OtherArgs>
-    __host__ __device__
-    auto operator()(OtherArgs&&... args)
-      -> decltype(
-           apply(
-             // XXX WAR nvcc 6.5 issue
-             //fun_,
-             *std::declval<F*>(),
-             substitute(
-               thrust::tie(std::forward<OtherArgs>(args)...),
-               // XXX WAR nvcc 6.5 issue
-               //bound_args_
-               *std::declval<thrust::tuple<BoundArgs...>*>()
-             )
-           )
-         )
-    {
-      return apply(
-        fun_,
-        substitute(
-          thrust::tie(std::forward<OtherArgs>(args)...),
-          bound_args_
-        )
-      );
-    }
-
-
-    __thrust_hd_warning_disable__
     template<class... OtherArgs>
     __host__ __device__
     auto operator()(OtherArgs&&... args) const
       -> decltype(
            apply(
              // XXX WAR nvcc 6.5 issue
-             //fun_,
-             *std::declval<const F*>(),
+             *std::declval<F*>(),
              substitute(
-               thrust::tie(std::forward<OtherArgs>(args)...),
+               forward_as_tuple(std::forward<OtherArgs>(args)...),
                // XXX WAR nvcc 6.5 issue
                //bound_args_
-               *std::declval<const thrust::tuple<BoundArgs...>*>()
+               *std::declval<tuple<BoundArgs...>*>()
              )
            )
          )
@@ -250,7 +457,7 @@ class bind_expression
       return apply(
         fun_,
         substitute(
-          thrust::tie(std::forward<OtherArgs>(args)...),
+          forward_as_tuple(std::forward<OtherArgs>(args)...),
           bound_args_
         )
       );
@@ -258,7 +465,7 @@ class bind_expression
 
   private:
     F fun_;
-    thrust::tuple<BoundArgs...> bound_args_;
+    tuple<BoundArgs...> bound_args_;
 };
 
 
--- demo.cu
+++ demo.cu
@@ -1,8 +1,10 @@
 #include "bind.hpp"
 #include <iostream>
 
-int main()
+__host__ __device__
+void demo()
 {
+  using namespace thrust::placeholders;
   using namespace thrust::experimental;
 
   auto return_13 = bind([]
@@ -10,23 +12,44 @@ int main()
     return 13;
   });
 
-  std::cout << "return_13: " << return_13() << std::endl;
-////
-////  auto add_1 = [](int x)
-////  {
-////    return x + 1;
-////  }
-////
-////  std::cout << "
-////
-//////  auto sum = [](int x, int y)
-//////  {
-//////    return x + y;
-//////  };
-//////
-//////  auto sum_two_and_three = bind(sum, 2, 3);
-////////
-////////  std::cout << "sum_two_and_three: " << sum_two_and_three() << std::endl;
+  printf("return_13: %d\n", return_13());
+
+  auto add_1 = bind([](int x)
+  {
+    return x + 1;
+  },
+  _1
+  );
+
+  printf("add_1(0): %d\n", add_1(0));
+
+  auto sum = [](int x, int y)
+  {
+    return x + y;
+  };
+
+  auto sum_two_and_three = bind(sum, 2, 3);
+
+  printf("sum_two_and_three: %d\n", sum_two_and_three());
+
+  auto bound_sum = bind(sum, _1, _2);
+
+  printf("bound_sum(2,3): %d\n", bound_sum(2,3));
+}
+
+__global__ void device_demo()
+{
+  printf("Demo on the device\n");
+  demo();
+}
+
+int main()
+{
+  printf("Demo on the host\n");
+  demo();
+
+  device_demo<<<1,1>>>();
+  cudaDeviceSynchronize();
 
   return 0;
 }
